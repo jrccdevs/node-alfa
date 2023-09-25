cefb096a449768b10a91bf66186ff4dfae436cbb
rei <reynaldocalani80@gmail.com>
2023-09-24 20:54:26 -0400

    agregando cors correccion2

--- index.js
+++ index.js
@@ -2,7 +2,12 @@ import app from './src/app.js'
 import { PORT } from "./src/config.js";
 import cors from 'cors'
 
-app.use(cors())
+const corsOrigin ={
+    origin:'http://localhost:3000', //or whatever port your frontend is using
+    credentials:true,            
+    optionSuccessStatus:200
+}
+app.use(cors(corsOrigin));
 
 app.listen(PORT);
 console.log(`Server is listening on port ${PORT}`);
