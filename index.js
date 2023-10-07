81b95b8fe0d1193fb71a62e12c2e74ca3c913e17
rei <reynaldocalani80@gmail.com>
2023-10-06 22:03:56 -0400

    cors solucion tres!

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 import app from './src/app.js'
 import { PORT } from "./src/config.js";
 
-
+app.use((req,res,next)=>{
+    res.setHeader('Access-Control-Allow-Origin','*');
+    res.setHeader('Access-Control-Allow-Methods','GET,POST,PUT,PATCH,DELETE');
+    res.setHeader('Access-Control-Allow-Methods','Content-Type','Authorization');
+    next(); 
+  })
 
 app.listen(PORT);
 console.log(`Server is listening on port ${PORT}`);
