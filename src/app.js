4ab1cbbbcf7e0dc283405b8884e90e363017df53
rei <reynaldocalani80@gmail.com>
2023-10-06 22:07:39 -0400

    cors solucion dos!!

--- src/app.js
+++ src/app.js
@@ -14,7 +14,12 @@ const app = express();
 //const __dirname = dirname(fileURLToPath(import.meta.url));
 //console.log(__dirname)
 
-
+app.use((req,res,next)=>{
+  res.setHeader('Access-Control-Allow-Origin','*');
+  res.setHeader('Access-Control-Allow-Methods','GET,POST,PUT,PATCH,DELETE');
+  res.setHeader('Access-Control-Allow-Methods','Content-Type','Authorization');
+  next(); 
+})
 app.use(express.json());
 
 
