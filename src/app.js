bd27d21c4a7e8675454ae3cdb2918ebeedcdb20d
rei <reynaldocalani80@gmail.com>
2023-10-07 22:40:25 -0400

    cors api json acces app

--- src/app.js
+++ src/app.js
@@ -19,9 +19,9 @@ const app = express();
 
 app.use(cors())
 app.use((req,res,next)=>{
-  res.header('Access-Control-Allow-Headers, *, Access-Control-Allow-Origin', 'Origin, X-Requested-with, Content_Type,Accept,Authorization','http://localhost:4200');
+  res.header('Access-Control-Allow-Headers, *, Access-Control-Allow-Origin', 'Origin, X-Requested-with, Content_Type,Accept,Authorization','https://reactaap.vercel.app');
   if(req.method === 'OPTIONS') {
-      res.header('Access-Control-Allow-Methods','PUT,POST,PATCH,DELETE,GET');
+      res.header('Access-Control-Allow-Methods','PUT,POST,PATCH,DELETE,GET,OPTIONS');
       return res.status(200).json({});
   }
   next();
